821f5d44c8fba61d6f62658c31a6079e64203963
Andrew Hamilton <ahamilton144@gmail.com>
2022-08-25 18:09:19 -0400

    install R GH actions

---
+++ docs/install_R_dependencies.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+### script for installing R dependencies from requirements file
+### simplified version of code here: https://stackoverflow.com/questions/54534153/install-r-packages-from-requirements-txt-file
+while IFS=" " read -r package;
+do
+	Rscript -e "install.packages('"$package"')";
+done < "requirements_r.txt"
