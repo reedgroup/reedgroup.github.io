f55031b128b8f962f5ff0fa5972d1a2c1ffc4828
Rohini Gupta <rg727@hopper.cac.cornell.edu>
2022-09-07 15:41:56 -0400

    modified dependencies file to have the right file location

--- docs/install_R_dependencies.sh
+++ docs/install_R_dependencies.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ### script for installing R dependencies from requirements file
-### simplified version of code here: https://stackoverflow.com/questions/54534153/install-r-packages-from-requirements-txt-file
+### simplified version of code here: https://stackoverflow.com/questions/54534153/install-r-packages-from-requirements-txt-files
 while IFS=" " read -r package;
 do
 	Rscript -e "install.packages('"$package"')";
-done < "/home/fs02/pmr82_0001/rg727/reedgroup.github.io/docs/requirements_r.txt"
+done < docs/requirements_r.txt"
