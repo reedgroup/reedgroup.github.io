089175b401945e228b98b9884b56da117f36350f
Andrew Hamilton <ahamilton144@gmail.com>
2023-06-27 23:27:18 -0400

    test manual html edit

--- docs/update_WaterProgramming_table_html.sh
+++ docs/update_WaterProgramming_table_html.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
-html_file=WaterProgramming/waterprogramming_post_catalog.html
-line=$(grep -n '<article class="bd-article" role="main"' 'docs/_build/html/'$html_file | tr ":" " " | tr "<" " " | tr ">" " " | tr '"' ' ')
-#line=($line)
-#line=${line[0]}
-#let line--
-#sed -i "${line}i <div><img src='_images/${image1}' width='50%'><img src='_images/${image2}' width='50%'></div> \n" '_build/html/'$html_file
-echo testing html edit
-echo $line
+html_file=docs/_build/html/WaterProgramming/waterprogramming_post_catalog.html
+### find line number matching this html snippet
+line=$(grep -n '<article class="bd-article" role="main"' $html_file | tr ":" " " | tr "<" " " | tr ">" " " | tr '"' ' ')
 line=($line)
 line=${line[0]}
-echo $line
+### now replace the full line with updated version that has height style
+sed -i '${line}s/.*/                <article class="bd-article" role="main" style="width: 100%; height: 100%;">/' $html_file
