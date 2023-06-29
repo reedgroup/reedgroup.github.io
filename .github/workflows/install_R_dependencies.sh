1210444c69664dee7c7107621440954560fd1497
Andrew Hamilton <ahamilton144@gmail.com>
2023-06-29 09:39:44 -0400

    moving images & GH action scripts to separate directories

---
+++ .github/workflows/install_R_dependencies.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+### script for installing R dependencies from requirements file
+### simplified version of code here: https://stackoverflow.com/questions/54534153/install-r-packages-from-requirements-txt-files
+while IFS=" " read -r package;
+do
+	Rscript -e "install.packages('"$package"')";
+done < "docs/requirements_r.txt"
