64f0a9916c6823a1329aec2c19df41f666178e33
unknown <lbl59@cornell.edu>
2025-04-21 16:39:21 -0400

    modifications to update_WP_table.sh

--- .github/workflows/update_WaterProgramming_table_html.sh
+++ .github/workflows/update_WaterProgramming_table_html.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #html_file=docs/_build/html/WaterProgramming/waterprogramming_post_catalog.html
-html_file=docs/WaterProgramming/waterprogramming_post_catalog.html
+html_file=docs/WaterProgramming/post_catalog.html
 ### find line number matching this html snippet
 line=$(grep -n 'article class="bd-article" role="main"' $html_file | tr ":" " " | tr "<" " " | tr ">" " " | tr '"' ' ')
 line=($line)
